0f66070de9cd65e0fcd9867df6a8fd086daba461
UlianaAsadulina <u.asadulina@icloud.com>
2025-02-23 19:01:36 -0500

    add route to submit addnew group button

--- src/components/AddGroupForm.jsx
+++ src/components/AddGroupForm.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
+import axios from "axios";
 
-export default function AddGroupForm({setShowForm}) {
-    const [groupName, setGroupName] = useState()
-    const [max, setMax] = useState()
-    
+export default function AddGroupForm({ setShowForm }) {
+    // const [groupName, setGroupName] = useState()
+    // const [max, setMax] = useState()
+    const [formData, setFormData] = useState({});
+
     function nameChange(e) {
-        setGroupName(e.target.value);
+        // setGroupName(e.target.value);
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
     function maxChange(e) {
-        setMax(e.target.value);
+        // setMax(e.target.value);
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     }
 
 
-    function handleSubmit(e) {
-        e.preventDefault();                    
-        setShowForm(false); // Hide the form after submission
-
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            let result = await axios.post('http://localhost:3000/groups', formData);
+            setShowForm(false); // Hide the form after submission
+        } catch (err) {
+            window.alert(err);
+            console.error(err);
+        }
     }
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="groupName"> Group name: 
-                <input type="text" name="groupName" id="groupName" onChange={nameChange}/>
+            <label htmlFor="group"> Group name:
+                <input type="text" name="group" onChange={nameChange} />
             </label>
-            <label htmlFor="maxKids"> Maximum children in group: 
-                <input type="text" name="maxKids" id="maxKids" onChange={maxChange}/>
+            <label htmlFor="kidsInGroup"> Maximum children in group:
+                <input type="text" name="kidsInGroup" onChange={maxChange} />
             </label>
             {/* <label htmlFor="teachers">
                 Teachers            
