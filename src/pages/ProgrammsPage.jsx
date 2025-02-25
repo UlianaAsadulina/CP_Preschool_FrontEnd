0427f91306ee29fdb0aeffebee100af8380edbee
UlianaAsadulina <u.asadulina@icloud.com>
2025-02-25 12:26:01 -0500

    page for programms

--- src/pages/ProgrammsPage.jsx
+++ src/pages/ProgrammsPage.jsx
@@ -4,15 +4,112 @@ export default function ProgrammsPage() {
             <h1>Our Programms</h1>
             <div className="programm">
                 <div className="programmImage">
-                    <img src="" alt="" />
+                    <img src="../../programmsImg/infant.jpg" alt="infant" />
                 </div>
-                <div>
+                <div className="proggramText">
+                    <h3>Infants (6wks-18months)</h3>
                     <p>
-                        Text
+                        Everyone knows the importance of schedules, even if the schedule is in an infant classroom.
+                        When your little one is in our care, you will discover that your infant's schedule at school is tailored to your schedule at home.
+                        Our experienced staff wants everything to be consistent with your routine, so tell us when to lay your baby down for a nap,
+                        your child's feeding schedule, and how often you would like his or her diaper changed and we will be happy to accommodate everything.
+                        Along with following your exact routine, our staff will also document everything that happened during your child's day so that you can
+                        know exactly how it went. Even though you weren't with your child during their day in our care, you will know all the details of his or
+                        her day with your personalized daily report.
+                        The only part of your baby's day we won't accommodate on is snuggling because we provide those all day long!
                     </p>
                 </div>
-
             </div>
+            <div className="programm">
+                <div className="proggramText" >
+                    <h3>Toddlers (18m-3yrs)</h3>
+                    <p>
+
+                        The best part about being a toddler is learning.
+                        The experienced staff at The Prescool allows your toddler to explore the different aspects of life during active play in a safe environment.
+                        The children in our toddler classroom love socializing during circle time, listening to stories, and singing songs with their friends
+                        during their day at The Preschool.
+                        Your children begin very basic self-help skills while in our care to help them grow into a responsible preschooler.
+                        They are expected to put their coats and backpacks into their personal cubbies as well as share with others and help clean up toys after playtime.
+                        The toddler room at The Preschool offers the perfect balance between quiet and interactive activities for your toddler who is always on the go.
+                    </p>
+                </div>
+                <div className="programmImage">
+                    <img src="../../programmsImg/toddler.jpg" alt="toddler" />
+                </div>
+            </div>
+            <div className="programm">
+                <div className="programmImage">
+                    <img src="../../programmsImg/preschool.jpg" alt="preschool group" />
+                </div>
+                <div className="proggramText">
+                    <h3>Preschool (3yrs-5yrs)</h3>
+                    <p>
+
+                        The theme-based curriculum at The Preschool promotes social, physical, cognitive, emotional, and
+                        creative development as your preschooler explores basic concepts of English language arts, mathematics, social studies, and science.
+                        Our childcare center is certified to provide your children with quality care and fully-licensed through the Ohio Department
+                        of Jobs and Family Services. Rest assured your children are in capable hands when you choose The Preschool for their preschool education.
+                        The qualified staff at The Preschool are there every step of the way to guide and support you and your child. So when that big first day
+                        of kindergarten is near, you will know that your child is prepared, even if you are not ready for your baby to grow up.
+
+                    </p>
+                </div>
+            </div>
+            <h3>Additional programms</h3>
+            <div className="programm">
+                <div className="proggramText" width="47%">
+                    <h3>Before and After School</h3>
+                    <p>
+
+                        School-age children will experience a very open-ended curriculum.
+                        We will also provide them with age appropriate activities to engage in before and after school.
+
+                        The children will assist the teachers in planning activities for when they are not in school, such as summer and holiday breaks.
+                        When the children are in school we provide them with materials and teacher support to assist them with their homework.
+
+
+                    </p>
+                </div>
+                <div className="proggramText">
+                    <h3>Summer Camp</h3>
+                    <p>
+
+
+                        The Preschool provides a Summer Camp Program for elementary school children.
+                        The camp consists of at least one field trip per week, along with fun and exciting adventures here at the center.
+                        The children will not only be exploring their community by going on field trips, but they will be participating in enrichment activities in reading, math and science.
+                        We will continue to further their education by engaging them in daily activities to keep their minds stimulated while still enjoying a fun, relaxing summer.
+
+                    </p>
+                </div>
+            </div>
+            <h3>Our partners</h3>
+            <div className="programm">
+                <div className="proggramText" width="50%">
+                    <img src="https://webbydancecompany.com/wp-content/uploads/2020/04/webby-logo.png" alt="webby logo" width="200px" />
+                    <br />
+                        <a href="https://webbydancecompany.com/">Webby Dance Company</a>
+
+                        <h5> Thursday: </h5>
+                        <p> 3:15 - 3:45 PM   Ballet Class </p>
+                        <p> 3:50 - 4:20 PM   Gymnastic Class </p>
+                        
+
+                </div>
+                <div className="proggramText">
+                    <img src="https://cincysc.com/wp-content/uploads/2021/11/website-header-cincysc-logo-over-wordmark-trans.png" alt="" width="200px" />
+                    <br />
+                    <a href="https://cincysc.com/"> Cincy Soccer</a>
+
+                    <h5> Wednesday: </h5>
+                    <p> 3:15 - 4:15 PM   Soccer </p>
+                    <br />
+
+                </div>
+            </div>
+            <h4>! IMPORTANT ! Classes requared additional registation and payment </h4>
+
         </>
     );
 }
