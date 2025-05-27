4cfe50c5d2667f6e01d2358bf0ca9c994010bfd2
UlianaAsadulina <u.asadulina@icloud.com>
2025-05-27 18:14:17 -0400

    add context to admin page

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,11 +1,22 @@
 import Form from "../components/Form";
 import "./AdminPage.css";
 import "./EditGroupPage.css"
+import { logout } from "../context/auth_context";
+import { useNavigate } from "react-router-dom";
+
+const { logout } = useAuth();
+const nav = useNavigate();
+
+function handleLogOut(e) {
+    logout();
+    nav('/auth');
+}
 
 export default function AdminPage() {
     return (
         <div className="admin">
             <h1>Administrator Page</h1>
+            <button onClick={handleLogOut}>LogOut</button>
             <Form className="adminForm" />
 
         </div>
