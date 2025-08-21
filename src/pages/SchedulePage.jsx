d0b965a4cc053c4f777d561ccc1e38dc441822ca
UlianaAsadulina <u.asadulina@icloud.com>
2025-08-21 15:16:11 -0400

    Enrollment forms added

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -29,13 +29,20 @@ export default function SchedulePage() {
         <h2>Enrollment</h2>
         <div className="proggramText">
             <p>Enrollment is based off a first come first serve basis.</p>
-        <p>We no longer have waiting list for either location. This page is updated at the beginning of each month. We try our best to keep this page updated, however we always recommend calling the center if you are interested in current openings.</p> 
-        <p>You may see an opening that fits your child's age however, different classrooms may have requirements, such as child must be potty trained, walking, non-mobile, etc. </p>
-        <p>If we have an opening that fits your families needs please call the center or set up a tour. After a tour, spots can be held with a registration fee of $50 for up to two weeks.</p>
+            <p>We no longer have waiting list for either location. This page is updated at the beginning of each month. We try our best to keep this page updated, however we always recommend calling the center if you are interested in current openings.</p> 
+            <p>You may see an opening that fits your child's age however, different classrooms may have requirements, such as child must be potty trained, walking, non-mobile, etc. </p>
+            <p>If we have an opening that fits your families needs please call the center or set up a tour. After a tour, spots can be held with a registration fee of $50 for up to two weeks.</p>
+            <p>
+                Enrolment documents: 
+                <a href="/public/documents/EnrollmentForm.pdf" target="_blank"> enrollment form</a>  
+                & 
+                <a href="/public/documents/MedicalForm.pdf" target="_blank"> medical form</a>
+            </p>
         </div>
         
         <Spots />
         <TourForm />
+       
         
     </>);
 
